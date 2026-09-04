7c3b5ce8a782c8e58208993c65c6d1fcc9e790fd
CHANCHAL YADAV <ychanchal9977@gmail.com>
2026-09-04 23:28:45 +0530

    Add SIZE_OF_DATATYPE.C to display data type sizes

---
+++ 01_BASICS/SIZE_OF_DATATYPE.C
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf("size of int:%u byte\n",sizeof(int));
+    printf("size of int:%u byte\n",sizeof(float));
+    printf("size of int:%u byte\n",sizeof(char));
+    printf("size of int:%u byte\n",sizeof(double));
+}
