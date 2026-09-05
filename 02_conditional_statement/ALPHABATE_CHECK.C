8714417aaa28ee0859eac6a84cb1e26b3411d961
CHANCHAL YADAV <ychanchal9977@gmail.com>
2026-09-05 22:42:26 +0530

    Add alphabet check program in ALPHABATE_CHECK.C

---
+++ 02_conditional_statement/ALPHABATE_CHECK.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter any character: ");
+    scanf("%c", &ch);
+
+    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+    {
+        printf("%c is an Alphabet.\n", ch);
+    } 
+    else 
+    {
+        printf("%c is Not an Alphabet.\n", ch);
+    }
+
+    return 0;
+}
