34378b2ae5cfca8426338e27ec1043dfbd894d00
CHANCHAL YADAV <ychanchal9977@gmail.com>
2026-09-14 20:47:16 +0530

    Add POSITIVE_NEGATIVE.C to evaluate number sign
    Implement a program to check if a number is positive, negative, or zero.

---
+++ 02_conditional_statement/POSITIVE_NEGATIVE.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main ()
+{   int num ;
+printf("enter a number:");
+scnaf("%d",&num);
+
+if(num>0)
+{ printf("IT IS A POSITIVE NUMBER:");
+}
+else if (num==0)
+{ 
+printf(" it is zero");
+}
+
+else{
+ printf(" IT IS A NEGATIVE NUMBER:");
+}
+}
