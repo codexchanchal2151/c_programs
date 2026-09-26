285bd1d83bb2def2e7246553c703190b4a38d889
CHANCHAL YADAV <ychanchal9977@gmail.com>
2026-09-26 21:08:40 +0530

    Add switch case for coffee and tea orders
    Implement a switch case structure for beverage selection.

---
+++ 04_SWITCH_CASE/FIXED_CUSM_ORDER.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+  int choice = 2;
+
+  // 1 = Coffee
+  // 2 = Tea
+
+  switch () {
+    case:
+      printf("You ordered Coffee");
+      break;
+
+    case:
+      printf("You ordered Tea");
+      break;
+
+      printf("Invalid choice");
+  }
+
+  return 0;
+}
